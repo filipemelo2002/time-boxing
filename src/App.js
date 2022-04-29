b11710caabbf9d282182b7e3daee15612be17ff4
Filipe Melo <filipe.melo@liferay.com>
2022-04-28 21:30:28 -0300

    TB-3 Create btn-light

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
       <button className="m-5 btn btn btn-primary">Save</button>
       <button className="m-5 btn btn-lg btn-primary">Save</button>
       <br />
+      <button className="m-5 btn btn-sm btn-light">Save</button>
+      <button className="m-5 btn btn btn-light">Save</button>
+      <button className="m-5 btn btn-lg btn-light">Save</button>
+      <br />
       <button className="m-5 btn btn-sm btn-dark">Save</button>
       <button className="m-5 btn btn-dark">Save</button>
       <button className="m-5 btn btn-lg btn-dark">Save</button>
