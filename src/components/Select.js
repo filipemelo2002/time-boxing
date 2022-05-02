aa6b20b9afca8ddb78a809a31baa789c118459aa
Filipe Melo <filipe.melo@liferay.com>
2022-05-02 20:58:34 -0300

    TB-4 Create initial select

---
+++ src/components/Select.js
@@ -0,0 +1,13 @@
+const Select = ({ data = [], value, onChange, className, ...props }) => {
+  return (
+    <select className={`form-control ${className}`} {...props}>
+      {data.map((item, index) => (
+        <option key={String(index)} value={item.value}>
+          {item.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default Select;
