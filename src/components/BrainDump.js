95f334648cba6c2c7adc54aae91ad0490b2c469b
Filipe Melo <filipe.melo@liferay.com>
2022-05-06 20:54:16 -0300

    TB-5 Create Brain Dump component

---
+++ src/components/BrainDump.js
@@ -0,0 +1,12 @@
+const BrainDump = () => {
+  return (
+    <div className="braindump">
+      <h4 className="text-white fw-bold border-2 border-bottom pb-3">
+        Brain Dump
+      </h4>
+      <textarea className="rounded-1"></textarea>
+    </div>
+  );
+};
+
+export default BrainDump;
