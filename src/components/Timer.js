8bd0a20f137a4d23d278050b05a26391f3041431
Filipe Melo <filipe.melo@liferay.com>
2022-05-22 14:23:23 -0300

    TB-8 Add Timer component HTML & Styling

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,5 +1,10 @@
 const Timer = () => {
-  return <h1>Timer Component</h1>;
+  return (
+    <div className="timer-component rounded-2 d-flex flex-column">
+      <h1 className="fw-bold text-light">15:00</h1>
+      <button className="btn btn-lg btn-primary mx-auto">Start</button>
+    </div>
+  );
 };
 
 export default Timer;
