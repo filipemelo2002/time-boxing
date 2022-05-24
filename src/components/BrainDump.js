20558c76a4327cc87e010bdc84a0f53d6f47db25
Filipe Melo <filipe.melo@liferay.com>
2022-05-24 20:56:42 -0300

    TB-21 Fix BrainDump styling classes

--- src/components/BrainDump.js
+++ src/components/BrainDump.js
@@ -1,6 +1,6 @@
 const BrainDump = () => {
   return (
-    <div className="braindump">
+    <div className="braindump mx-auto">
       <h4 className="text-white fw-bold border-2 border-bottom pb-3">
         Brain Dump
       </h4>
