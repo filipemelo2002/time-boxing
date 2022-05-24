12558d6494c8234cbe44f575ead4ae9bc6848c5a
Filipe Melo <filipe.melo@liferay.com>
2022-05-24 20:58:27 -0300

    TB-21 Fix Timer styling classes

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,7 +1,7 @@
 const Timer = () => {
   return (
     <div className="timer-component rounded-2 d-flex flex-column">
-      <h1 className="fw-bold text-light">15:00</h1>
+      <h1 className="fw-bold text-light mx-auto">15:00</h1>
       <button className="btn btn-lg btn-primary mx-auto">Start</button>
     </div>
   );
