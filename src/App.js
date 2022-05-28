5940edf7c2e5f3960ed30885e143301d4f7cc3b2
Filipe Melo <filipe.melo@liferay.com>
2022-05-28 16:51:49 -0300

    TB-22 Move context function calls to within the components

--- src/App.js
+++ src/App.js
@@ -3,28 +3,18 @@ import TopPriorities from "./components/TopPrioritites";
 import Timer from "./components/Timer";
 import Activities from "./components/Activities";
 import { ReactComponent as Logo } from "./assets/logo.svg";
-import { useTimeBoxing } from "./contexts/TimeBoxingContext";
 
 function App() {
-  const { brainDump, topPriorities, onAddTopPriority, onChangeBrainDump } =
-    useTimeBoxing();
-
   return (
     <>
       <div className="d-grid template-columns-2 m-auto px-2 gap-3">
         <div className="d-flex flex-column align-items-center gap-4 justify-content-center">
           <Logo className="mt-4 me-auto img-logo img-logo-container" />
           <Timer />
-          <TopPriorities
-            topPriorities={topPriorities}
-            onAddTopPriority={onAddTopPriority}
-          />
+          <TopPriorities />
           <Activities />
         </div>
-        <BrainDump
-          value={brainDump}
-          onChange={(event) => onChangeBrainDump(event.target.value)}
-        />
+        <BrainDump />
       </div>
     </>
   );
--- src/components/TopPrioritites.js
+++ src/components/TopPrioritites.js
@@ -1,21 +1,28 @@
+import { useTimeBoxing } from "../contexts/TimeBoxingContext";
 import ActivityItem from "./ActivityItem";
 import AddTaskButton from "./AddTaskButton";
 
-const TopPriorities = ({ topPriorities = [], onAddTopPriority }) => {
+const TopPriorities = () => {
+  const { topPriorities, onAddTopPriority, onRemoveTopPriority } =
+    useTimeBoxing();
+
   return (
     <div className="top-prioritites">
       <div className="d-flex w-100 border-2 border-bottom">
         <h4 className="text-white fw-bold  pb-3">Top Prioritites</h4>
-        <span className="text-white ms-auto fw-bold">1/3</span>
+        <span className="text-white ms-auto fw-bold">
+          {topPriorities.length}/3
+        </span>
       </div>
 
       <div className="d-flex flex-column gap-3 mt-3">
-        {topPriorities.map((topPriority, index) => (
+        {topPriorities.map((topPriority) => (
           <ActivityItem
-            key={index}
+            key={topPriority.id}
             title={topPriority.title}
             description={topPriority.description}
             done={topPriority.done}
+            onRemove={() => onRemoveTopPriority(topPriority.id)}
           />
         ))}
       </div>
