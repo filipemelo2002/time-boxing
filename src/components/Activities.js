2d69ca89f7e7fd0df965c893af5c1be86f3dfa4c
Filipe Melo <filipe.melo@liferay.com>
2022-05-28 18:12:09 -0300

    TB-22 Add time selector logic

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import ActivityItem from "./ActivityItem";
 import AddTaskButton from "./AddTaskButton";
 import Select from "./Select";
 
 const Activities = () => {
+  const [time, setTime] = useState(new Date().getHours());
   return (
     <div className="activities-component w-100">
       <div className="d-flex border-2 border-bottom">
@@ -11,31 +13,33 @@ const Activities = () => {
         <Select
           className="form-select select select-primary w-auto ms-auto mb-2"
           data={[
-            { name: "00:00", value: "0" },
-            { name: "01:00", value: "1" },
-            { name: "02:00", value: "2" },
-            { name: "03:00", value: "3" },
-            { name: "04:00", value: "4" },
-            { name: "05:00", value: "5" },
-            { name: "06:00", value: "6" },
-            { name: "07:00", value: "7" },
-            { name: "08:00", value: "8" },
-            { name: "09:00", value: "9" },
-            { name: "10:00", value: "10" },
-            { name: "11:00", value: "11" },
-            { name: "12:00", value: "12" },
-            { name: "13:00", value: "13" },
-            { name: "14:00", value: "14" },
-            { name: "15:00", value: "15" },
-            { name: "16:00", value: "16" },
-            { name: "17:00", value: "17" },
-            { name: "18:00", value: "18" },
-            { name: "19:00", value: "19" },
-            { name: "20:00", value: "20" },
-            { name: "21:00", value: "21" },
-            { name: "22:00", value: "22" },
-            { name: "23:00", value: "23" },
+            { name: "00:00", value: 0 },
+            { name: "01:00", value: 1 },
+            { name: "02:00", value: 2 },
+            { name: "03:00", value: 3 },
+            { name: "04:00", value: 4 },
+            { name: "05:00", value: 5 },
+            { name: "06:00", value: 6 },
+            { name: "07:00", value: 7 },
+            { name: "08:00", value: 8 },
+            { name: "09:00", value: 9 },
+            { name: "10:00", value: 10 },
+            { name: "11:00", value: 11 },
+            { name: "12:00", value: 12 },
+            { name: "13:00", value: 13 },
+            { name: "14:00", value: 14 },
+            { name: "15:00", value: 15 },
+            { name: "16:00", value: 16 },
+            { name: "17:00", value: 17 },
+            { name: "18:00", value: 18 },
+            { name: "19:00", value: 19 },
+            { name: "20:00", value: 20 },
+            { name: "21:00", value: 21 },
+            { name: "22:00", value: 22 },
+            { name: "23:00", value: 23 },
           ]}
+          value={time}
+          onChange={(event) => setTime(event.target.value)}
         />
       </div>
       <div className="d-flex flex-column gap-3 mt-3">
