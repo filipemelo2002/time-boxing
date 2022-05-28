7af827a980d4d7316317239ab1c62c7510a3339e
Filipe Melo <filipe.melo@liferay.com>
2022-05-28 18:12:09 -0300

    TB-22 Add time selector logic

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,6 +1,11 @@
 const Select = ({ data = [], value, onChange, className, ...props }) => {
   return (
-    <select className={`form-control ${className}`} {...props}>
+    <select
+      className={`form-control ${className}`}
+      value={value}
+      onChange={onChange}
+      {...props}
+    >
       {data.map((item, index) => (
         <option key={String(index)} value={item.value}>
           {item.name}
