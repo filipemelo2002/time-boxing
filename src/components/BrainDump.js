31c6a8709c06ebfc16d8bb786d5aaebda85c5580
Filipe Melo <filipe.melo@liferay.com>
2022-05-28 16:51:49 -0300

    TB-22 Move context function calls to within the components

--- src/components/BrainDump.js
+++ src/components/BrainDump.js
@@ -1,10 +1,17 @@
-const BrainDump = ({ onChange, value }) => {
+import { useTimeBoxing } from "../contexts/TimeBoxingContext";
+
+const BrainDump = () => {
+  const { brainDump, onChangeBrainDump } = useTimeBoxing();
   return (
     <div className="braindump mx-auto">
       <h4 className="text-white fw-bold border-2 border-bottom pb-3">
         Brain Dump
       </h4>
-      <textarea className="rounded-1" onChange={onChange} value={value} />
+      <textarea
+        className="rounded-1"
+        onChange={(event) => onChangeBrainDump(event.target.value)}
+        value={brainDump}
+      />
     </div>
   );
 };
