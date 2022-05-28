19fd8762a628590f37967d4432e4f730250da4de
Filipe Melo <filipe.melo@liferay.com>
2022-05-28 11:28:27 -0300

    TB-22 Add activity Item internal logic

--- src/components/ActivityItem.js
+++ src/components/ActivityItem.js
@@ -1,21 +1,34 @@
+import { useState } from "react";
 import { ReactComponent as Icon } from "../assets/check.svg";
 
-const ActivityItem = ({ done }) => {
+const ActivityItem = ({ done, title, description, duration }) => {
+  const [openDescription, setOpenDescription] = useState(false);
   return (
-    <div className="activity-item d-flex border-left bg-white rounded p-3 align-items-center">
-      <h4
-        className={`fw-bold m-0 ${
-          done ? "text-decoration-line-through text-gray" : ""
-        }`}
-      >
-        Lorem ipsum dolor...
-      </h4>
+    <div
+      className="activity-item border-left d-flex flex-column gap-3 bg-white rounded p-3 "
+      onClick={() => setOpenDescription(!openDescription)}
+    >
+      <div className="d-flex align-items-center">
+        <h4
+          className={`fw-bold m-0 ${
+            done ? "text-decoration-line-through text-gray" : ""
+          }`}
+        >
+          {title}
+        </h4>
 
-      <button
-        className={`btn p-0 ms-auto ${done ? "btn-primary" : "btn-light"}`}
-      >
-        <Icon />
-      </button>
+        <button
+          className={`btn p-0 ms-auto ${done ? "btn-primary" : "btn-light"}`}
+        >
+          <Icon />
+        </button>
+      </div>
+      {openDescription && (
+        <>
+          <p>{description}</p>
+          <button className="btn btn-sm btn-dark me-auto">Delete</button>
+        </>
+      )}
     </div>
   );
 };
