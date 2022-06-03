639757fc32d076aca92064f2c59c015ba6c6fded
Filipe Melo <filipe.melo@liferay.com>
2022-06-03 20:40:41 -0300

    TB-25 Move convertTimeToMMSS function to Utils

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -23,15 +23,6 @@ const Timer = () => {
     setIsCountingDown(!isCountingDown);
   };
 
-  const convertTimeToMMSS = (value) => {
-    if (!value) return "--:--";
-    const minutes = Math.floor(value / 60);
-    const seconds = value % 60;
-    return `${minutes < 10 ? `0${minutes}` : minutes}:${
-      seconds < 10 ? `0${seconds}` : seconds
-    }`;
-  };
-
   useEffect(() => {
     if (isCountingDown) {
       const timer = setTimeout(() => {
