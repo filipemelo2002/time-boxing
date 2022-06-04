9f18ff30b5f0820f1265cb435af6cf0eef74c972
Filipe Melo <filipe.melo@liferay.com>
2022-06-03 21:32:59 -0300

    TB-25 Delete unnecessary constants

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,10 +1,3 @@
-export const ACTIVITY_STATUS = {
-  CREATED: 0,
-  STARTED: 1,
-  PAUSED: 2,
-  FINISHED: 3,
-};
-
 export const ACTIVITY_TYPES = {
   TOP_PRIORITIES: "topPriorities",
   ACTIVITIES: "activities",
